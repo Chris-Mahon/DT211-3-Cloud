710fb0b543709b3701e57c555b19e02ba3d3c7d4
Christopher Mahon <chris-mahon@hotmail.com>
2014-10-02 15:19:12 +0000

    Completed Euler 4

--- Euler/Euler4.py
+++ Euler/Euler4.py
@@ -1,13 +1,20 @@
+"""Euler 4-
+Highest Palindrome with a 3 digit number
+The code consists of 2 for loops, which goes through every possible combination of number. 
+It then checks if the product is the same backwards as it is forwards and adds it to a number that represents the highest palindrone that has occurred"""
 i=0
 j=0
 reverse=0
+Highest = 0
+
 print("Hello")
-for i in range(0, 999):
-  for j in range(0, 999):
+for i in range(0, 1000):
+  for j in range(0, 1000):
     Number=i*j
-    print(Number, str(Number)[::-1])
-    if str(Number)[::-1] is Number:
-      print("Reverse!")
-      reverse+=1
+    if int(str(Number)[::-1]) == Number:
+      if Highest < Number:
+        Highest = Number 
+      elif Highest == 0:
+        Highest = Number
       
-print("Times reversed is ", reverse)
+print("Highest Palindrone is", Highest)
